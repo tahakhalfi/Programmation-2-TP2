a35da7ee18a58b948c07c84f4e65aec5f2427d09
tata <taha.khalfi.ob@gmail.com>
2025-12-21 20:32:43 -0500

    Added Chronologue structure

--- src/main/java/manager/Chronologue.java
+++ src/main/java/manager/Chronologue.java
@@ -3,9 +3,25 @@ package manager;
 import javafx.animation.PauseTransition;
 import javafx.util.Duration;
 
+import java.time.LocalDate;
+
 public class Chronologue {
 
-    public static void freeze(double seconds) {
+    public static String date() {
+
+        LocalDate today = LocalDate.now();
+
+        int day = today.getDayOfMonth();
+        int month = today.getMonthValue();
+        int year = today.getYear();
+
+        String format = day + "/" + month + "/" + year;
+
+        return format;
+
+    }
+
+    public static void pause(double seconds) {
 
         long milliseconds = (long) (1000 * seconds);
 
