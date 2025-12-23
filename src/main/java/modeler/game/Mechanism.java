89129ce6398f31ddd3f876189e91a787c147c76a
tata <taha.khalfi.ob@gmail.com>
2025-12-22 23:39:58 -0500

    Added Mechanism Changes

--- src/main/java/modeler/game/Mechanism.java
+++ src/main/java/modeler/game/Mechanism.java
@@ -4,13 +4,27 @@ import java.util.LinkedList;
 
 public class Mechanism {
 
+    private static LinkedList<Player> players = new LinkedList<>();
+
     private static boolean active = false;
     private static int turn = -1;
 
-    private static LinkedList<Player> players = new LinkedList<>();
+    public static void place(Player player) {
 
-    public static void sign(Player player) {
-        players.add(player);
+        players.addLast(player);
+
+    }
+
+    public static void eject() {
+
+        players.removeLast();
+
+    }
+
+    public static void purge() {
+
+        players.clear();
+
     }
 
     public static void start() {
@@ -25,13 +39,47 @@ public class Mechanism {
 
         active = true;
 
-    }
-
-    public static void move() {
+        move(0);
 
     }
 
-    public static void alert() {
+    public static void move(int focus) {
+
+        if (players.isEmpty()) {
+            return;
+        }
+
+        if (focus <= -1 || focus >= players.size()) {
+            return;
+        }
+
+        Player control = getPlayer(focus);
+
+        if (control == null) {
+            return;
+        }
+
+        turn = focus;
+
+    }
+
+    public static void shut() {
+
+        turn = -1;
+
+        end();
+
+    }
+
+    public static void pass() {
+
+        int next = turn + 1;
+
+        if (next < players.size()) {
+            move(next);
+        } else {
+            shut();
+        }
 
     }
 
@@ -47,6 +95,28 @@ public class Mechanism {
 
         active = false;
 
+        purge();
+
+    }
+
+    public static boolean getActive() {
+        return active;
+    }
+
+    public static int getTurn() {
+        return turn;
+    }
+
+    public static LinkedList<Player> getPlayers() {
+        return players;
+    }
+
+    public static Player getPlayer(int index) {
+        return players.get(index);
+    }
+
+    public static Player getControl() {
+        return getPlayer(turn);
     }
 
 }
